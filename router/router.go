8fce9019e912ea7a0e32b962f02bb13652fdb898
Mingjun Lim <limmingjun@ip-10-1-4-166.us-west-2.compute.internal>
2019-06-17 18:10:41 -0700

    removed mux

--- router/router.go
+++ router/router.go
@@ -3,10 +3,9 @@ package router
 import (
 	"github.com/geofence/controller"
 	"net/http"
-	"github.com/gorilla/mux"
 )
 
-func InitRoutes(router *mux.Router) *mux.Router {
+func InitRoutes() {
 	http.HandleFunc("/circle/", controller.CircleHandler)
 	http.HandleFunc("/poly/", controller.PolyHandler)
 }
