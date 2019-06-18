e90112fd068578bbeb2b74df2df4d3dd6d012cf9
Mingjun Lim <limmingjun@ip-10-1-4-166.us-west-2.compute.internal>
2019-06-17 20:53:04 -0700

    edit gopkg.toml

--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"github.com/geofence/router"
 	"log"
 	"net/http"
-
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":8080"
+	}
 	router.InitRoutes()
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(port, nil))
 }
