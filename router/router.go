e7fd5c9e670f6575a2a4230e3e905fe37abb58bd
Mingjun Lim <limmingjun@ip-10-1-4-166.us-west-2.compute.internal>
2019-06-17 17:45:24 -0700

    add dep

--- router/router.go
+++ router/router.go
@@ -3,9 +3,10 @@ package router
 import (
 	"github.com/geofence/controller"
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
-func InitRoutes() {
+func InitRoutes(router *mux.Router) *mux.Router {
 	http.HandleFunc("/circle/", controller.CircleHandler)
 	http.HandleFunc("/poly/", controller.PolyHandler)
 }
